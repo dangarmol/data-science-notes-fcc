716a64bdfbcda29dd612b1eaf258a732c63d28dd
Daniel G <dangar13@icloud.com>
2020-11-26 23:35:05 +0100

    Time Series Visualizer line plot finished

--- Data Analysis with Python/Projects/Page View Time Series Visualiser/time_series_visualizer.py
+++ Data Analysis with Python/Projects/Page View Time Series Visualiser/time_series_visualizer.py
@@ -5,18 +5,23 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=True, index_col="date")
 
 # Clean data
-df = None
+df = df[(df["value"] >= df["value"].quantile(0.025)) & (df["value"] <= df["value"].quantile(0.975))]
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, axis = plt.subplots(1, 1)
 
+    fig.set_figwidth(15)
+    fig.set_figheight(5)
 
-
-
+    plt.plot(df, color="#CF0500", linewidth=.9, antialiasing=True)
+    plt.title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+    plt.xlabel("Date")
+    plt.ylabel("Page Views")
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -24,6 +29,7 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     df_bar = None
 
     # Draw bar plot
