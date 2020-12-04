bb5ffaa2e085ef9496415bb51d13b0749e0becd8
Daniel G <dangar13@icloud.com>
2020-12-04 01:24:14 +0100

    Added code for Bar Plot. Code works in Jupyter but not PyCharm 🤔

--- Data Analysis with Python/Projects/Page View Time Series Visualiser/time_series_visualizer.py
+++ Data Analysis with Python/Projects/Page View Time Series Visualiser/time_series_visualizer.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=True, index_col="date")
+df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=True)
+df.set_index('date', drop=False)
 
 # Clean data
 df = df[(df["value"] >= df["value"].quantile(0.025)) & (df["value"] <= df["value"].quantile(0.975))]
@@ -18,7 +20,7 @@ def draw_line_plot():
     fig.set_figwidth(15)
     fig.set_figheight(5)
 
-    plt.plot(df, color="#CF0500", linewidth=.9, antialiasing=True)
+    plt.plot(df, color="#CF0500", linewidth=.9)
     plt.title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
     plt.xlabel("Date")
     plt.ylabel("Page Views")
@@ -29,14 +31,46 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    df_bar = None
+    # Inspired by https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/barchart.html
 
-    # Draw bar plot
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+              "November", "December"]
 
+    df["year"] = pd.DatetimeIndex(df["date"]).year
+    df["month"] = pd.DatetimeIndex(df["date"]).month
 
+    df_grouped = df.groupby(["year", "month"]).mean().copy().reset_index()
 
+    group_labels = pd.unique(df["year"])
 
+    means = list()
+
+    for i in range(1, 13):
+        means.append(list(df_grouped[(df_grouped["month"] == i)]["value"]))
+        if len(means[i - 1]) < 4:
+            means[i - 1].insert(0, 0)
+
+    x = np.arange(len(group_labels))  # the year label locations
+    width = 0.05  # the width of the bars
+
+    fig, axis = plt.subplots(figsize=(10, 13 * (2 / 3)))
+
+    rects = list()
+    size_differential = -11
+
+    for i in range(0, 12):
+        rects.append(axis.bar(x + ((size_differential + (2 * i)) / 2) * width, means[i], width, label=months[i]))
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    axis.set_xlabel("Years", fontsize=14)
+    axis.set_ylabel("Average Page Views", fontsize=14)
+    axis.set_xticks(x)
+    axis.set_xticklabels(group_labels, rotation=90, ha="center")
+    axis.legend(title="Months", title_fontsize="x-large", fontsize="x-large")
+
+    fig.tight_layout()
+
+    plt.show()
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
